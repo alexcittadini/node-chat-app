1a31883349d0fcc9d6ac50e4a32c5c1cc61007e6
Alex Cittadini <Alex@Alexs-MacBook-Pro.local>
2017-08-23 17:32:34 +1000

    Added new user in room alerts

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    to: 'jen@1234.com',
-    text: 'hey'
-  })
 });
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -13,15 +13,29 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=> {
   console.log('New user connected');
-
-  socket.emit('newMessage', {
-    from: 'alex@123.com',
-    text: 'test text',
-    createAt: new Date(),
+  socket.emit('newMessage',{
+    from: 'admin',
+    text: 'Welcome to chat-app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from: 'admin',
+    text: 'New User in room',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
+    io.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
